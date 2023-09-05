05de37d96efa5d91f37f577d9f65ec360a05eef5
Ariel Castro <arielmatiascastro@gmail.com>
2023-09-04 22:10:25 -0300

    InmuService Frontend
    InmuService

---
+++ Frontend/src/services/InmuebleService.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const URL = "http://localhost:8080/api/inmueble/";
+
+class InmuebleService {
+    ingresarInmueble(inmuebleForm, token) {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}` }
+        };
+        return axios.post(URL + "registroInmueble", inmuebleForm, config);
+  }
+}
+export default new InmuebleService();
